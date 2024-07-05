4f90a430cc94f06a60e82c93238389bf4bce8a72
M.P Usama <136558201+Usamafuward@users.noreply.github.com>
2024-07-05 22:39:42 +0530

    Pradhesiya shaba app is built and the landing page workis almost done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import '../styles/Home.css'
+import app from '../assets/app.png'
+
+const Home = () => {
+  return (
+    <div className='home container1' style={{ paddingTop:"35px"}}>
+        <div className="home-text">
+            <h1>Your concerns, <br/>Our promise</h1>
+            <p>Register and Connect with your local council easily.<br/> Use our app to send complaints, ask questions, and book services. <br/>Download our Mobile App now and get involved in your community!</p>
+            <div className="button-container">
+              <button className="btn1">Register Now</button>
+              <button className="btn fill">Download App</button>
+            </div>   
+        </div>
+
+            <div className="app">
+              <img src={app} alt="app" />
+            </div>
+
+    </div>
+  );
+};
+
+export default Home;
+
+
+
+// const Home = () => {
+//   return (
+//     <div className='home container1'>
+//       <header>
+//         <nav className="navbar">
+//           <div className="home-text">
+//             <h1>Your concerns, <br/>Our promise</h1>
+//             <p>Register and Connect with your local council easily.<br/> Use our app to send complaints, ask questions, and book services. <br/>Download our Mobile App now and get involved in your community!</p>
+//             <div className="button-container">
+//               <button className="btn1">Register Now</button>
+//               <button className="btn fill">Download App</button>
+//             </div>   
+//           </div>
+//         </nav>
+//       </header>
+//       <div className="app">
+//         <img src={app} alt="app" />
+//       </div>
+//     </div>
+//   );
+// };
+
