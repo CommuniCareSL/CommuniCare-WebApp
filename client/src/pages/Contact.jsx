3623b255ad0b4852955d1fa29f8a0de6aebe20b0
ucsakura1213 <ucsakura1213@gmail.com>
2024-07-11 16:38:43 +0530

    landingpage changes

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import '../styles/Contact.css';
+import React from "react";
+import "../styles/Contact.css";
 
 const Contact = () => {
   return (
     <div className="contact-body" style={{ paddingTop: "60px" }}>
       {/* <nav className='container'><img src={logo} alt="Logo" className='logo'/></nav> */}
-      <div className="contact-wrapper">
+      <div className="contact-wrapper" style={{marginLeft:'20%'}}>
         <form action="#">
           <h2>CONTACT US</h2>
 
-          <div className="contact-input-field">
-            <input type="text" required />
-            <label>Name</label>
+          <div style={{ display: "flex" }}>
+            <div className="contact-input-field">
+              <input type="text" required />
+              <label>First Name</label>
+            </div>
+            <div className="contact-input-field">
+              <input type="text" required />
+              <label>Last Name</label>
+            </div>
           </div>
-          <div className="contact-input-field">
-            <input type="email" required />
-            <label>Email</label>
+
+          <div style={{ display: "flex" }}>
+            <div className="contact-input-field">
+              <input type="phone" required />
+              <label>Phone Number</label>
+            </div>
+            <div className="contact-input-field">
+              <input type="email" required />
+              <label>Email</label>
+            </div>
           </div>
-          <div className="contact-input-field">
-            <input type="phone" required />
-            <label>Phone Number</label>
-          </div>
-          <div className="contact-input-field">
-            <input type="text" required />
-            <label>Description</label>
+
+          <br />
+          
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <label style={{ marginRight: "84%" }}>Description</label>
+
+            <textarea
+              rows="5"
+              cols="50"
+              required
+              className="textarea"
+            ></textarea>
           </div>
 
           <button type="submit">SEND</button>
@@ -31,6 +49,6 @@ const Contact = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Contact;
