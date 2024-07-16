dbc0fea36bd46733aee9c8a0007cd7c3d8922e1d
ucsakura1213 <ucsakura1213@gmail.com>
2024-07-16 06:43:05 +0530

    Dashboard sidebar page Completion

---
+++ src/components/Dashboard/Sidebar.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import './Dashboard.css';
+import logo from '../../assets/DarkLogo.png';
+import profileImg from '../../assets/profile-img.jpg';
+
+const Dashboard = () => {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="DarkLogo.png" />
+        {/* <h2>Communi Care</h2> */}
+      </div>
+      <ul className="sidebar-links">
+        <h4>
+          {/* <span>Main Menu</span> */}
+          <div className="menu-separator"></div>
+        </h4>
+        <li>
+          <a href="#">
+            <span className="material-symbols-outlined">dashboard</span>
+            Dashboard
+          </a>
+        </li>
+       
+        <li>
+          <a href="#">
+            <span className="material-symbols-outlined">report_problem</span>
+            Complaints
+          </a>
+        </li>
+        <h4>
+          {/* <span>General</span> */}
+          {/* <div className="menu-separator"></div> */}
+        </h4>
+        <li>
+          <a href="#">
+            <span className="material-symbols-outlined">build</span>
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="material-symbols-outlined">overview</span>
+            Report
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="material-symbols-outlined">person</span>
+            Profile
+          </a>
+        </li>
+       
+        {/* <li>
+          <a href="#">
+            <span className="material-symbols-outlined">notifications_active</span>
+            Notifications
+          </a>
+        </li> */}
+       
+        {/* <li>
+          <a href="#">
+            <span className="material-symbols-outlined">account_circle</span>
+            Profile
+          </a>
+        </li> */}
+
+        {/* <li>
+          <a href="#">
+            <span className="material-symbols-outlined">settings</span>
+            Settings
+          </a>
+        </li> */}
+        <br /><br /><br /><br />
+        <br /><br /><br />
+       
+        <li>            
+          <a href="#">
+            <span className="material-symbols-outlined">logout</span>
+            Logout
+          </a>
+        </li>
+      </ul>
+      <div className="user-account">
+        <div className="user-profile">
+          <img src={profileImg} alt="Profile Image" />
+          <div className="user-detail">
+            <h3>MANOKARARAJAH</h3>
+            <span>Admin</span>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Dashboard;
