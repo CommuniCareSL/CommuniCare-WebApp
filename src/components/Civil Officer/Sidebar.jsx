95d488607d8639281d8d3c74a08755fc34597de1
Raveesha Weerasinghe <136602532+Raveesha-dw@users.noreply.github.com>
2024-07-20 00:14:53 +0530

    sidebar changes for officer

---
+++ src/components/Civil Officer/Sidebar.jsx
@@ -0,0 +1,25 @@
+import logo from '../../assets/Admin/DarkLogo.png';
+import '../../styles/components/Civil officer/Sidebar.css';
+import profileImg from '../../assets/Admin/profile-img.jpg';
+import { BsArrowLeftShort } from "react-icons/bs";
+
+const Sidebar = () => {
+    return(
+        <div className='sidebar-officer'>
+        <BsArrowLeftShort className='icon-arrow'/>
+        </div>
+
+
+
+
+
+
+
+
+        // <aside>
+        //     <h3>Navigation</h3>
+        // </aside>
+    );
+};
+
+export default Sidebar
---
+++ src/pages/Civil Officer/Dashboard.jsx
@@ -0,0 +1,17 @@
+import Sidebar from '../../components/Civil Officer/Sidebar';
+
+const Dashboard = () => {
+    return (
+        <div className='flex'>
+        <Sidebar />
+        <div className='p-7'>
+            <h1 className='bg-blue text-2xl font-semibold'>Homepage-Officer</h1>
+            
+        </div>
+        
+        
+        </div>
+    );
+};
+
+export default Dashboard;
