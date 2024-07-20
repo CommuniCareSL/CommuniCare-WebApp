c98afe41ec7f91c68052f1594341e8a0a8a02799
sakunika <muthunihansani052@gmail.com>
2024-07-20 17:46:44 +0530

    Begining the card slider for the admin dashboard

---
+++ src/components/Admin/CardSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardSlider = () => {
+  return (
+    <div className="admin-dashboard-box-details-row-view">
+          
+    </div>
+  )
+}
+
+export default CardSlider
