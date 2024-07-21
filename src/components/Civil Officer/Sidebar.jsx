16f380fb75563cb6c1155e83b1d1821612e3e79f
Raveesha Weerasinghe <136602532+Raveesha-dw@users.noreply.github.com>
2024-07-21 22:38:57 +0530

    sidebar for officer

--- src/components/Civil Officer/Sidebar.jsx
+++ src/components/Civil Officer/Sidebar.jsx
@@ -1,13 +1,56 @@
 import logo from '../../assets/Admin/DarkLogo.png';
 import '../../styles/components/Civil officer/Sidebar.css';
 import profileImg from '../../assets/Admin/profile-img.jpg';
-import { BsArrowLeftShort } from "react-icons/bs";
+import { BsArrowLeftShort , BsSearch} from "react-icons/bs";
+import { RiDashboardFill, RiCalendar2Line, RiServiceLine, RiBarChart2Fill, RiProfileLine, RiLogoutBoxLine, RiFileList2Line } from 'react-icons/ri';
+import { useState } from 'react';
+
+const Menus = [
+    { title: 'Home', icon:<RiDashboardFill />  },
+    { title: 'Calendar Schedule', icon:<RiCalendar2Line /> },
+    { title: 'Appointments', icon:<RiFileList2Line /> },
+    { title: 'Service Requests', icon:<RiServiceLine /> },
+    { title: 'Analytics',icon:<RiBarChart2Fill /> },
+    { title: 'Profile', icon:<RiProfileLine />, spacing:true},
+    { title: 'Logout', icon:<RiLogoutBoxLine />},
+
+  ];
 
 const Sidebar = () => {
+    const [open, setOpen] = useState(true);
     return(
-        <div className='sidebar-officer'>
-        <BsArrowLeftShort className='icon-arrow'/>
+        <div className={`sidebar-officer ${open ? 'sidebar-open' : 'sidebar-closed'}`}>
+        <BsArrowLeftShort className={`icon-arrow ${!open ? 'icon-rotate' : ''}`} onClick={() => setOpen(!open)}/>
+        
+        <div className='icons-sidebar'>
+        <img src={logo} alt="logo" className="logo-img" />
+
         </div>
+
+        <div className='searchSidebar'>
+        <BsSearch className='search-icon'/>
+
+        <input type={"search"} placeholder='Search' className={`searchBar ${!open ? 'searchBar-hidden' : ''}`}/>
+        </div>
+
+        <ul className='Menu-items-officer'>
+            {Menus.map((menu, index) => (
+                <>
+                <li key={index} className={`list-items ${menu.spacing ? 'menu-icon-itemSpacing' : ''}`}>
+                    <span className='menu-icon'>
+                        {/* <RiDashboardFill /> */}
+                        {menu.icon}
+                    </span>
+                    <span className={`menu-icon-item ${!open ? 'menu-icon-item-hidden' : ''}`}>{menu.title}</span>
+                </li>
+                </>
+            ))}
+
+        </ul>
+        
+        </div>
+
+        
 
 
 
