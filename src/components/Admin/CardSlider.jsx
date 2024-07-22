54b45afbf383313b0a19d19f7fc838c537ca7f8d
sakunika <muthunihansani052@gmail.com>
2024-07-22 18:47:38 +0530

    add a progressbar to cards

--- src/components/Admin/CardSlider.jsx
+++ src/components/Admin/CardSlider.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Slider from "react-slick";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -10,7 +11,7 @@ const CardSlider = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1
   };
 
@@ -20,29 +21,107 @@ const CardSlider = () => {
 
         <div className="card-slider-section-view-cards">
           <div className="card-slider-section-view-cards-title">
-            <span className="material-symbols-outlined">dashboard</span><h3>Renting Service</h3>
+            <span className="material-symbols-outlined">dashboard</span>
+            <h3>Renting Service</h3>
           </div>
           <div className="card-slider-section-view-cards-details">
             <p>Remaining Service Requests  :</p><h5>40</h5>
           </div>
           <div className="card-slider-section-view-cards-progress-bar">
-             
+           <ProgressBar completed={60} bgColor='#0991FF' animateOnRender={true} labelSize='10px' height='12px'/>
           </div>
         </div>
 
-        <div className="card-slider-section-view-cards">Title 2</div>
+        <div className="card-slider-section-view-cards">
+          <div className="card-slider-section-view-cards-title">
+            <span className="material-symbols-outlined">dashboard</span>
+            <h3>Renting Service</h3>
+          </div>
+          <div className="card-slider-section-view-cards-details">
+            <p>Remaining Service Requests  :</p><h5>40</h5>
+          </div>
+          <div className="card-slider-section-view-cards-progress-bar">
+           <ProgressBar completed={40} bgColor='#0991FF' animateOnRender={true} labelSize='10px' height='12px'/>
+          </div>
+        </div>
 
-        <div className="card-slider-section-view-cards">Title 3</div>
+        <div className="card-slider-section-view-cards">
+          <div className="card-slider-section-view-cards-title">
+            <span className="material-symbols-outlined">dashboard</span>
+            <h3>Renting Service</h3>
+          </div>
+          <div className="card-slider-section-view-cards-details">
+            <p>Remaining Service Requests  :</p><h5>40</h5>
+          </div>
+          <div className="card-slider-section-view-cards-progress-bar">
+           <ProgressBar completed={80} bgColor='#0991FF' animateOnRender={true} labelSize='10px' height='12px'/>
+          </div>
+        </div>
 
-        <div className="card-slider-section-view-cards">Title 4</div>
+        <div className="card-slider-section-view-cards">
+          <div className="card-slider-section-view-cards-title">
+            <span className="material-symbols-outlined">dashboard</span>
+            <h3>Renting Service</h3>
+          </div>
+          <div className="card-slider-section-view-cards-details">
+            <p>Remaining Service Requests  :</p><h5>40</h5>
+          </div>
+          <div className="card-slider-section-view-cards-progress-bar">
+           <ProgressBar completed={10} bgColor='#0991FF' animateOnRender={true} labelSize='10px' height='12px'/>
+          </div>
+        </div>
 
-        <div className="card-slider-section-view-cards">Title 5</div>
+        <div className="card-slider-section-view-cards">
+          <div className="card-slider-section-view-cards-title">
+            <span className="material-symbols-outlined">dashboard</span>
+            <h3>Renting Service</h3>
+          </div>
+          <div className="card-slider-section-view-cards-details">
+            <p>Remaining Service Requests  :</p><h5>40</h5>
+          </div>
+          <div className="card-slider-section-view-cards-progress-bar">
+           <ProgressBar completed={20} bgColor='#0991FF' animateOnRender={true} labelSize='10px' height='12px'/>
+          </div>
+        </div>
 
-        <div className="card-slider-section-view-cards">Title 6</div>
+        <div className="card-slider-section-view-cards">
+          <div className="card-slider-section-view-cards-title">
+            <span className="material-symbols-outlined">dashboard</span>
+            <h3>Renting Service</h3>
+          </div>
+          <div className="card-slider-section-view-cards-details">
+            <p>Remaining Service Requests  :</p><h5>40</h5>
+          </div>
+          <div className="card-slider-section-view-cards-progress-bar">
+           <ProgressBar completed={50} bgColor='#0991FF' animateOnRender={true} labelSize='10px' height='12px'/>
+          </div>
+        </div>
 
-        <div className="card-slider-section-view-cards">Title 7</div>
+        <div className="card-slider-section-view-cards">
+          <div className="card-slider-section-view-cards-title">
+            <span className="material-symbols-outlined">dashboard</span>
+            <h3>Renting Service</h3>
+          </div>
+          <div className="card-slider-section-view-cards-details">
+            <p>Remaining Service Requests  :</p><h5>40</h5>
+          </div>
+          <div className="card-slider-section-view-cards-progress-bar">
+           <ProgressBar completed={90} bgColor='#0991FF' animateOnRender={true} labelSize='10px' height='12px'/>
+          </div>
+        </div>
 
-        <div className="card-slider-section-view-cards">Title 8</div>
+        <div className="card-slider-section-view-cards">
+          <div className="card-slider-section-view-cards-title">
+            <span className="material-symbols-outlined">dashboard</span>
+            <h3>Renting Service</h3>
+          </div>
+          <div className="card-slider-section-view-cards-details">
+            <p>Remaining Service Requests  :</p><h5>40</h5>
+          </div>
+          <div className="card-slider-section-view-cards-progress-bar">
+           <ProgressBar completed={70} bgColor='#0991FF' animateOnRender={true} labelSize='10px' height='12px'/>
+          </div>
+        </div>
 
       </Slider>
 
