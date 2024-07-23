abbc50c51f6c623c780df24424d06681499c869f
sakunika <muthunihansani052@gmail.com>
2024-07-23 19:16:13 +0530

    adding welcome bar to admin dashboard

--- src/components/Admin/CardSlider.jsx
+++ src/components/Admin/CardSlider.jsx
@@ -10,12 +10,39 @@ import rentImage from '../../assets/Admin/rent.png';
 import BookingImage from '../../assets/Admin/booking.png';
 
 const CardSlider = () => {
-  const settings = {
+  var settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      // {
+      //   breakpoint: 480,
+      //   settings: {
+      //     slidesToShow: 1,
+      //     slidesToScroll: 1
+      //   }
+      // }
+    ]
   };
 
   return (
