6f1b5ae97c0b63555eae55d61e2673d13d9bcc37
sakunika <muthunihansani052@gmail.com>
2024-07-27 14:36:57 +0530

    tab-view-for-complaints

--- src/components/Admin/TabTableView.jsx
+++ src/components/Admin/TabTableView.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../styles/components/Admin/TabTableView.css';
+
 function TabTableView() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
   return (
-    <div>
-      
+    <div className="tabs-or-status-of-complaints">
+      <ul className="tab-list">
+        <li className={activeTab === 0 ? 'active' : ''} onClick={() => handleTabClick(0)}>Unseen</li>
+        <li className={activeTab === 1 ? 'active' : ''} onClick={() => handleTabClick(1)}>Inprocess</li>
+        <li className={activeTab === 2 ? 'active' : ''} onClick={() => handleTabClick(2)}>Completed</li>
+      </ul>
+      <div className="tab-content">
+        {activeTab === 0 && <div className="content-for-the-status active slide-in">
+          
+          </div>}
+        {activeTab === 1 && <div className="content-for-the-status active slide-in">
+          
+          </div>}
+        {activeTab === 2 && <div className="content-for-the-status active slide-in">
+          
+          </div>}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TabTableView
-
+export default TabTableView;
