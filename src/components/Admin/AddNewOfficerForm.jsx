f161bdee9c2bee8bf8ee473e72ca7481dcb9292f
sakunika <muthunihansani052@gmail.com>
2024-07-31 09:26:40 +0530

    add new officer form add a dropdown list

--- src/components/Admin/AddNewOfficerForm.jsx
+++ src/components/Admin/AddNewOfficerForm.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddNewOfficerForm() {
+    const [selectedValue, setSelectedValue] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
   return (
     <div className='add-new-officer-form-content'>
         <div className="add-new-officer-form-Name">
@@ -14,6 +20,20 @@ function AddNewOfficerForm() {
 
         <div className="add-new-officer-address">
             <textarea name="" id="" placeholder='Address'></textarea>
+        </div>
+
+        <div className="add-new-officer-selection-for-position">
+            <select id="simple-select" value={selectedValue} onChange={handleChange}
+                style={{
+                color: selectedValue ? 'black' : '#a2a2a2', // Set color based on selectedValue
+                }}
+            >
+                <option value="" disabled className='placeholder-selcet' style={{color:'#a2a2a2'}}>Please choose the Position</option>
+                <option value="option1">Service 1</option>
+                <option value="option2">service 2</option>
+                <option value="option3">service 3</option>
+                <option value="option4">service 4</option>
+            </select>
         </div>
 
     </div>
