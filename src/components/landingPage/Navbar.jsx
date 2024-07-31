92422f3e352fbc6a9ce0b84739c6f49d3457d5a8
AshenSachinthana <dasachinthana@gmail.com>
2024-07-31 09:40:42 +0530

    Resolve navbar CSS issue

--- src/components/landingPage/Navbar.jsx
+++ src/components/landingPage/Navbar.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="container">
+    <nav className="navcontainer">
       <div className="logo-container">
         <img src={Logo} alt="Logo" className="logo" />
       </div>
