9d749563d9028dea599aad6b3fe01c14ddab6aff
sakunika <muthunihansani052@gmail.com>
2024-07-31 10:18:33 +0530

    add new officer form css changes

--- src/components/Admin/AddedNewOfficers.jsx
+++ src/components/Admin/AddedNewOfficers.jsx
@@ -9,7 +9,7 @@ function AddedNewOfficers() {
         <tbody>
 
             <a href="">
-              <tr className='admin-add-new-officer-table-row' style={{marginTop: '4vh'}}>
+              <tr className='admin-add-new-officer-table-row' style={{marginTop: '6vh'}}>
                 <td className='admin-add-new-officer-table-row-column-1'>
                   <img src={profileImg} alt="Profile Image" />
                 </td>
