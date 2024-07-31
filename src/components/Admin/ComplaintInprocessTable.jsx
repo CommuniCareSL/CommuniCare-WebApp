2aa353e701d5161c15aae9c88b4ad190b260ce60
sakunika <muthunihansani052@gmail.com>
2024-07-31 15:43:55 +0530

    admin complain complaints changes

--- src/components/Admin/ComplaintInprocessTable.jsx
+++ src/components/Admin/ComplaintInprocessTable.jsx
@@ -24,7 +24,7 @@ const ComplaintInprocessTable = () => {
                             {/* <button><span class="material-symbols-outlined">visibility</span></button> */}
                         </td>
                         <td className="complaint-table-with-details-row-column-5">
-                            <button onClick={(e) => e.stopPropagation()}><span class="material-symbols-outlined">task_alt</span></button>
+                            <button><span class="material-symbols-outlined">arrow_forward_ios</span></button>
                         </td>
                     </tr>
                 </a>
@@ -45,7 +45,7 @@ const ComplaintInprocessTable = () => {
                             {/* <button><span class="material-symbols-outlined">visibility</span></button> */}
                         </td>
                         <td className="complaint-table-with-details-row-column-5">
-                            <button onClick={(e) => e.stopPropagation()}><span class="material-symbols-outlined">task_alt</span></button>
+                            <button><span class="material-symbols-outlined">arrow_forward_ios</span></button>
                         </td>
                     </tr>
                 </a>
@@ -66,7 +66,7 @@ const ComplaintInprocessTable = () => {
                             {/* <button><span class="material-symbols-outlined">visibility</span></button> */}
                         </td>
                         <td className="complaint-table-with-details-row-column-5">
-                            <button onClick={(e) => e.stopPropagation()}><span class="material-symbols-outlined">task_alt</span></button>
+                            <button><span class="material-symbols-outlined">arrow_forward_ios</span></button>
                         </td>
                     </tr>
                 </a>
@@ -87,7 +87,91 @@ const ComplaintInprocessTable = () => {
                             {/* <button><span class="material-symbols-outlined">visibility</span></button> */}
                         </td>
                         <td className="complaint-table-with-details-row-column-5">
-                            <button onClick={(e) => e.stopPropagation()}><span class="material-symbols-outlined">task_alt</span></button>
+                            <button><span class="material-symbols-outlined">arrow_forward_ios</span></button>
+                        </td>
+                    </tr>
+                </a>
+
+                <a href="/AdminInprogressComplainDetails" onClick={(e) => handleClick(e, '/AdminInprogressComplainDetails')}>
+                    <tr className="complaint-table-with-details-row">
+                        <td className="complaint-table-with-details-row-column-1">
+                            <img src={profileImg} alt="Profile Image" />
+                        </td>
+                        <td className="complaint-table-with-details-row-column-2">
+                            <h3>user name</h3>
+                            <p>complaint submitted date</p>
+                        </td>
+                        <td className="complaint-table-with-details-row-column-3">
+                            <p>Complaint title/Category</p>
+                        </td>
+                        <td className="complaint-table-with-details-row-column-4">
+                            {/* <button><span class="material-symbols-outlined">visibility</span></button> */}
+                        </td>
+                        <td className="complaint-table-with-details-row-column-5">
+                            <button><span class="material-symbols-outlined">arrow_forward_ios</span></button>
+                        </td>
+                    </tr>
+                </a>
+
+                <a href="/AdminInprogressComplainDetails" onClick={(e) => handleClick(e, '/AdminInprogressComplainDetails')}>
+                    <tr className="complaint-table-with-details-row">
+                        <td className="complaint-table-with-details-row-column-1">
+                            <img src={profileImg} alt="Profile Image" />
+                        </td>
+                        <td className="complaint-table-with-details-row-column-2">
+                            <h3>user name</h3>
+                            <p>complaint submitted date</p>
+                        </td>
+                        <td className="complaint-table-with-details-row-column-3">
+                            <p>Complaint title/Category</p>
+                        </td>
+                        <td className="complaint-table-with-details-row-column-4">
+                            {/* <button><span class="material-symbols-outlined">visibility</span></button> */}
+                        </td>
+                        <td className="complaint-table-with-details-row-column-5">
+                            <button><span class="material-symbols-outlined">arrow_forward_ios</span></button>
+                        </td>
+                    </tr>
+                </a>
+
+                <a href="/AdminInprogressComplainDetails" onClick={(e) => handleClick(e, '/AdminInprogressComplainDetails')}>
+                    <tr className="complaint-table-with-details-row">
+                        <td className="complaint-table-with-details-row-column-1">
+                            <img src={profileImg} alt="Profile Image" />
+                        </td>
+                        <td className="complaint-table-with-details-row-column-2">
+                            <h3>user name</h3>
+                            <p>complaint submitted date</p>
+                        </td>
+                        <td className="complaint-table-with-details-row-column-3">
+                            <p>Complaint title/Category</p>
+                        </td>
+                        <td className="complaint-table-with-details-row-column-4">
+                            {/* <button><span class="material-symbols-outlined">visibility</span></button> */}
+                        </td>
+                        <td className="complaint-table-with-details-row-column-5">
+                            <button><span class="material-symbols-outlined">arrow_forward_ios</span></button>
+                        </td>
+                    </tr>
+                </a>
+
+                <a href="/AdminInprogressComplainDetails" onClick={(e) => handleClick(e, '/AdminInprogressComplainDetails')}>
+                    <tr className="complaint-table-with-details-row">
+                        <td className="complaint-table-with-details-row-column-1">
+                            <img src={profileImg} alt="Profile Image" />
+                        </td>
+                        <td className="complaint-table-with-details-row-column-2">
+                            <h3>user name</h3>
+                            <p>complaint submitted date</p>
+                        </td>
+                        <td className="complaint-table-with-details-row-column-3">
+                            <p>Complaint title/Category</p>
+                        </td>
+                        <td className="complaint-table-with-details-row-column-4">
+                            {/* <button><span class="material-symbols-outlined">visibility</span></button> */}
+                        </td>
+                        <td className="complaint-table-with-details-row-column-5">
+                            <button><span class="material-symbols-outlined">arrow_forward_ios</span></button>
                         </td>
                     </tr>
                 </a>
