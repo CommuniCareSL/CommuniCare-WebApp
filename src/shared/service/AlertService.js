0c9cea7f43c42a972123a5763772133ea3ce62f1
AshenSachinthana <dasachinthana@gmail.com>
2025-01-13 23:53:54 +0530

    Update AlertService button styles for improved UI consistency

--- src/shared/service/AlertService.js
+++ src/shared/service/AlertService.js
@@ -67,10 +67,10 @@ class AlertService {
       customClass: {
         confirmButton:
           options.confirmButtonClass ||
-          'bg-buttonBlue text-white px-6 py-2 rounded-md text-m mr-4 w-auto',
+          'bg-blue-500 text-white px-6 py-2 rounded-md text-m mr-4 w-auto',
         cancelButton:
           options.cancelButtonClass ||
-          'bg-white border border-gray-400 text-gray-800 px-6 py-2 rounded-md text-m w-auto',
+          'bg-gray-400 border border-gray-400 text-gray-800 px-6 py-2 rounded-md text-m w-auto',
       },
     });
   }
